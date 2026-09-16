e092020f6942ddc29656d94ee1105c59b89e05ad
Alok Patel <alok89123patel@gmail.com>
2026-09-16 08:22:45 +0530

    Questions

--- src/main/java/com/JavaDsaQuestions/problemSolvingDSA/Collections/Filters.java
+++ src/main/java/com/JavaDsaQuestions/problemSolvingDSA/Collections/Filters.java
@@ -1,6 +1,10 @@
 package com.JavaDsaQuestions.problemSolvingDSA.Collections;
 
+import java.awt.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Filters {
@@ -34,21 +38,43 @@ public class Filters {
 		System.out.println(maxNumber);
 	}
 	
+//  Sort a List
+	private void sortList(ArrayList<Integer> nums) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> sortList = nums.stream().sorted()
+				.collect(Collectors.toCollection(ArrayList::new));
+		System.out.println("Sorted List" + sortList);
+		
+	}
+//  Sort a List - in reverse Order - .sorted(Comparator.reverseOrder())
+	private void sortInReverse(ArrayList<Integer> nums) {
+		ArrayList<Integer> desSort = nums.stream().sorted(Comparator.reverseOrder())
+				.collect(Collectors.toCollection(ArrayList::new));
+		System.out.println("reverseSorting desSort - " + desSort.toString());
+		
+	}
+	
+	
 	public static void main(String []args) {
 		Filters filterObj = new Filters();
 		ArrayList<Integer> nums = new ArrayList<>();
-		nums.add(12);
-		nums.add(22);
-		nums.add(11);
-		nums.add(-1);
-		nums.add(-3);
-		nums.add(-92);
-		nums.add(77);
+		nums.add(90);
+		// inex 1 se sare element isme inset karega
+		nums.addAll(1, nums);
+		nums.add(100);
+		System.out.println(nums);
+//		nums.addAll(Arrays.asList(12, 22, 11, -1, -3, -92, 77));
 		// filter OddEven Numbers from List
+		
 		filterObj.evenOdd(nums);
 		filterObj.oddList(nums);
 		
 		//Find Maximum in list
 		filterObj.finMaxElement(nums);
+		//  Sort a List
+		filterObj.sortList(nums);
+		// sortInReverse
+		filterObj.sortInReverse(nums);
+		
 	}
 }
